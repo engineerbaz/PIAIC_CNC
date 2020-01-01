b155bfda1dacba4c502437557e4a175f6a5fbe79
Bashir Ahmed Zeeshan <56934817+engineerbaz@users.noreply.github.com>
2020-01-01 20:42:02 +0500

    Update task1.js

--- assignment/java/assignment3_cnc026886/task1.js
+++ assignment/java/assignment3_cnc026886/task1.js
@@ -3,5 +3,6 @@
 var edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M-Phil", "PhD"];
 for (var i=0; i<edu.length; i++) { 
   document.write(i+1+")  "+edu[i]+" <br>");
-  document.body.innerHTML(i+1+")  "+edu[i]+" <br>"); 
+  alert(i+1+")  "+edu[i]+" <br>");
+  // document.body.innerHTML(i+1+")  "+edu[i]+" <br>"); 
   }
