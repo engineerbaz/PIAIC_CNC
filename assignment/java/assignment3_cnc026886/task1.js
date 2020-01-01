fde8f748a6940121863fce3527e470478b5caedd
Bashir Ahmed Zeeshan <56934817+engineerbaz@users.noreply.github.com>
2020-01-01 20:30:23 +0500

    Update task1.js

--- assignment/java/assignment3_cnc026886/task1.js
+++ assignment/java/assignment3_cnc026886/task1.js
@@ -1,10 +1,15 @@
-<h1> JavaScript Programming Assignment 3 </h1>
+<h2> Assignment # 03 </h2>
+<h4> Declare and Initialize an array and store available education
+qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M.
+Phil., PhD). Show the listed qualifications in your browser like: </h4>
 
-#### 1. Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M.Phil., PhD). Show the listed qualifications in your browser like:
-
-```js
+<script>
 var edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M-Phil", "PhD"];
 for (var i=0; i<edu.length; i++) { 
   document.write(i+1+")  "+edu[i]+" <br>");
   }
-```
+
+
+  
+
+</script> 
