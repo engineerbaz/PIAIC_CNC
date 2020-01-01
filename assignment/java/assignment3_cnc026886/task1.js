c49cbeafe9e7eeecd3d814e92afe4b875ebbdc86
Bashir Ahmed Zeeshan <56934817+engineerbaz@users.noreply.github.com>
2020-01-01 20:37:28 +0500

    Update task1.js

--- assignment/java/assignment3_cnc026886/task1.js
+++ assignment/java/assignment3_cnc026886/task1.js
@@ -1,11 +1,6 @@
 
-<script>
+
 var edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M-Phil", "PhD"];
 for (var i=0; i<edu.length; i++) { 
   document.write(i+1+")  "+edu[i]+" <br>");
   }
-
-
-  
-
-</script> 
