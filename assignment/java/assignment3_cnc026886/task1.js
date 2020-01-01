07370768b9d98f10117d35d520ba5f232c29bf97
Bashir Ahmed Zeeshan <56934817+engineerbaz@users.noreply.github.com>
2020-01-01 20:28:57 +0500

    Update and rename assignment/java/assignment3_cnc026886.md to assignment/java/assignment3_cnc026886/task1.js

--- assignment/java/assignment3_cnc026886/task1.js
+++ assignment/java/assignment3_cnc026886/task1.js
@@ -1,3 +1,11 @@
 # JavaScript Programming Assignment 3
 
 #### 1. Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M.Phil., PhD). Show the listed qualifications in your browser like:
+
+```js
+
+var edu = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M-Phil", "PhD"];
+for (var i=0; i<edu.length; i++) { 
+  document.write(i+1+")  "+edu[i]+" <br>");
+  }
+```
